79e31bc1f3c08d624f5e214c84eb780d721406d0
cmarr31 <chris@sitefuelusa.com>
2017-07-31 23:09:58 -0500

    Picking up where I left off on making_dictionaries.py

---
+++ python_post_defer/python_fundamentals/making_dictionaries.py
@@ -0,0 +1,7 @@
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+def make_dictionary(arr1, arr2):
+	new_dict = {}
+	##
+	return new_dict
