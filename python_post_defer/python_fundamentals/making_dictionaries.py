39f5288c39fa299f3108e933f751bc8aefd3a816
cmarr31 <chris@sitefuelusa.com>
2017-08-01 17:23:41 -0500

    making_dictionaries assignment.

--- python_post_defer/python_fundamentals/making_dictionaries.py
+++ python_post_defer/python_fundamentals/making_dictionaries.py
@@ -2,6 +2,16 @@ name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
 favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
 
 def make_dictionary(arr1, arr2):
+	keys = []
+	values = []
+	if len(arr1)>=len(arr2):
+		keys = arr1
+		values = arr2
+	else:
+		keys = arr2
+		values = arr1
 	new_dict = {}
-	##
+	for x in range(len(keys)):
+		new_dict[arr1[x]] = arr2[x]
 	return new_dict
+print make_dictionary(name, favorite_animal)
