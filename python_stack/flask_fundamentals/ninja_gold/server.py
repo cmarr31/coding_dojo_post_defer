4ab5c1af390e29e91a1c67e3494e388922cce186
cmarr31 <chris@sitefuelusa.com>
2017-08-07 13:57:29 -0500

    .

--- python_stack/flask_fundamentals/ninja_gold/server.py
+++ python_stack/flask_fundamentals/ninja_gold/server.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
-app.secret_key = 'asdf'
+app.secret_key = 'ninjagold'
+
 
 @app.route('/')
 def index():
+	if 'gold' is not in session:
+		session['gold'] = 0
+	else:
+		session['gold'] +=1
 	return render_template('index.html')
+
+@app.route('/process_money')
+def process_money():
+	session_gold = form.session['gold']
+	return redirect('/')
 
 
 
